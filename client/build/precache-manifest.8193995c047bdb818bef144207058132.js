21418eb9f13755f940fffc1e76195e05abfdd52f
Peter Geyser <peterpgeyser@gmail.com>
2019-11-02 11:08:08 -0700

    Created production files

---
+++ client/build/precache-manifest.8193995c047bdb818bef144207058132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c56f267a8a182c5ee7ad",
+    "url": "/static/js/main.e7dd3daf.chunk.js"
+  },
+  {
+    "revision": "e117ad32674202ac001d",
+    "url": "/static/js/2.ea250b03.chunk.js"
+  },
+  {
+    "revision": "c56f267a8a182c5ee7ad",
+    "url": "/static/css/main.941281c7.chunk.css"
+  },
+  {
+    "revision": "f4acc48599182b6fa3c5ad8a9f298cc7",
+    "url": "/index.html"
+  }
+];
